1e249f0ae1aee35366d4f5ae323364801b67200a
SamyAguilar <samiaguilar321@gmail.com>
2024-12-11 14:24:52 -0600

    controlador, modelo, repositorio de user
    se agregaron y completaron las clases para los usuarios

---
+++ api/src/main/java/com/example/api/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.api.repositories;
+
+
+import com.example.api.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ api/src/main/java/com/example/api/services/UserService.java
@@ -0,0 +1,51 @@
+package com.example.api.services;
+
+
+import com.example.api.models.User;
+import com.example.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // Listar todas las personas
+    public List<User> getAllUsuarios() {
+        return userRepository.findAll();
+    }
+
+    // Buscar persona por ID
+    public Optional<User> getUsuariosById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    // Crear nueva persona
+    public User createUsuarios(User usuario) {
+        return userRepository.save(usuario);
+    }
+
+    // Actualizar una persona
+    public Optional<User> updateUsuarios(Long id, User userDetails) {
+        return userRepository.findById(id).map(user -> {
+            user.setNombre(userDetails.getNombre());
+            user.setApellido(userDetails.getApellido());
+            user.setEdad(userDetails.getEdad());
+            return userRepository.save(user);
+        });
+    }
+
+    // Eliminar una persona
+    public boolean deleteUsuario(Long id) {
+        if (userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
