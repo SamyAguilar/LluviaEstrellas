4823ef3285fd91403670d018dded5617be963822
SamyAguilar <samiaguilar321@gmail.com>
2024-12-11 16:40:51 -0600

    actualizacion 2.1
    actualizacion de creacion de modelo para tabla de BD

--- api/src/main/java/com/example/api/controllers/UserController.java
+++ api/src/main/java/com/example/api/controllers/UserController.java
@@ -1,31 +1,26 @@
 package com.example.api.controllers;
 
 import com.example.api.models.User;
+import com.example.api.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
-import com.example.api.services.UserService;
-//package com.example.sqlapi.controllers;
-
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
-@RequestMapping("/api/usuarios")
+@RequestMapping("/api/user")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    // Listar todas las personas
     @GetMapping
     public List<User> getAllUsuarios() {
         return userService.getAllUsuarios();
     }
 
-    // Obtener una persona por ID
     @GetMapping("/{id}")
     public ResponseEntity<User> getPersonaById(@PathVariable Long id) {
         return userService.getUsuariosById(id)
@@ -33,13 +28,11 @@ public class UserController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    // Crear una nueva persona
     @PostMapping
     public User createUsuarios(@RequestBody User user) {
         return userService.createUsuarios(user);
     }
 
-    // Actualizar una persona
     @PutMapping("/{id}")
     public ResponseEntity<User> updateUsuario(@PathVariable Long id, @RequestBody User userDetails) {
         return userService.updateUsuarios(id, userDetails)
@@ -47,7 +40,6 @@ public class UserController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    // Eliminar una persona
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePersona(@PathVariable Long id) {
         if (userService.deleteUsuario(id)) {
