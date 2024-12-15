1dc88d8ba3c9e475308e436e57ce305dedd64c10
MartinezMiguelAlexis <20161266@itoaxaca.edu.mx>
2024-12-14 21:05:41 -0600

    Interfaz de login funcional

--- interfaz/LluviaPalabras/src/main.jsx
+++ interfaz/LluviaPalabras/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import RegistroPalabras from './components/RegistroPalabras';
+import InicioJuego from './components/InicioJuego';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/registro-palabras" element={<RegistroPalabras />} />
+        <Route path="/inicio-juego" element={<InicioJuego />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
